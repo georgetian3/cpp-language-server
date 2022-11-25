74173a7c12beee58e0fb932ca51b4a8bf0a34564
George Tian <github@crowdlabel.org>
2022-11-25 19:11:38 +0800

    n4860 + keywords

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
 """
-2.3 Character sets
+5.3 Character sets
 
 The basic source character set consists of 96 characters: the space character, the control characters representing horizontal tab, vertical tab, form feed, and new-line, plus the following 91 graphical characters:
 a b c d e f g h i j k l m n o p q r s t u v w x y z
@@ -11,6 +11,13 @@ _ { } [ ] # ( ) < > % : ; . ? * + - / ^ & | ~ ! = , \ " ’
 basic_source_character_set = set(
     ' \t\v\f\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_{}[]#()<>%:;.?*+-/^&|~!=,\\"\''
 )
+
+
+keywords = [
+    'alignas', 'alignof', 'asm', 'auto', 'bool', 'break', 'case', 'catch', 'char', 'char16_t', 'char32_t', 'char8_t', 'class', 'co_await', 'co_return', 'co_yield', 'concept', 'const', 'const_cast', 'consteval', 'constexpr', 'constinit', 'continue', 'decltype', 'default', 'delete', 'do', 'double', 'dynamic_cast', 'else', 'enum', 'explicit', 'export', 'extern', 'false', 'float', 'for', 'friend', 'goto', 'if', 'inline', 'int', 'long', 'mutable', 'namespace', 'new', 'noexcept', 'nullptr', 'operator', 'private', 'protected', 'public', 'register', 'reinterpret_cast', 'requires', 'return', 'short', 'signed', 'sizeof', 'static', 'static_assert', 'static_cast', 'struct', 'switch', 'template', 'this', 'thread_local', 'throw', 'true', 'try', 'typedef', 'typeid', 'typename', 'union', 'unsigned', 'using', 'virtual', 'void', 'volatile', 'wchar_t', 'while'
+]
+
+
 
 if __name__ == '__main__':
     print(basic_source_character_set)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,35 @@
 from lex import lex
 
 """
-Several comments below are excerpts from the 2022 Working Draft
+Several comments in this repository originate from the C++ Working Draft N4860
 """
 
+
 """
-2.6 Tokens
+5.6 Tokens
 
 There are five kinds of tokens: identifiers, keywords, literals, 18 operators, and other separators. Blanks, horizontal and vertical tabs, newlines, formfeeds, and comments (collectively, “white space”), as described below, are ignored except as they serve to separate tokens. [ Note: Some white space is required to separate otherwise adjacent identifiers, keywords, numeric literals, and alternative tokens containing alphabetic characters. — end note ]
 """
 
+
+"""
+5.10 Identifiers
+
+identifier :
+    identifier-nondigit
+    identifier identifier-nondigit
+    identifier digit
+identifier-nondigit :
+    nondigit
+    universal-character-name
+nondigit : one of
+    a b c d e f g h i j k l m
+    n o p q r s t u v w x y z
+    A B C D E F G H I J K L M
+    N O P Q R S T U V W X Y Z _
+digit : one of
+    0 1 2 3 4 5 6 7 8 9
+"""
 
 
 tokens = (
