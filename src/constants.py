242cdfdea2ddcda631abb194f38d17f222ddf315
George Tian <github@crowdlabel.org>
2022-11-25 17:57:06 +0800

    character set replacement

---
+++ src/constants.py
@@ -0,0 +1,18 @@
+"""
+2.3 Character sets
+
+The basic source character set consists of 96 characters: the space character, the control characters representing horizontal tab, vertical tab, form feed, and new-line, plus the following 91 graphical characters:
+a b c d e f g h i j k l m n o p q r s t u v w x y z
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+0 1 2 3 4 5 6 7 8 9
+_ { } [ ] # ( ) < > % : ; . ? * + - / ^ & | ~ ! = , \ " ’
+"""
+
+basic_source_character_set = set(
+    ' \t\v\f\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_{}[]#()<>%:;.?*+-/^&|~!=,\\"\''
+)
+
+if __name__ == '__main__':
+    print(basic_source_character_set)
+    for char in basic_source_character_set:
+        print(ord(char))
--- src/main.py
+++ src/main.py
@@ -1,12 +1,74 @@
-import lex
+from lex import lex
+
+"""
+Several comments below are excerpts from the 2022 Working Draft
+"""
+
+"""
+2.6 Tokens
+
+There are five kinds of tokens: identifiers, keywords, literals, 18 operators, and other separators. Blanks, horizontal and vertical tabs, newlines, formfeeds, and comments (collectively, “white space”), as described below, are ignored except as they serve to separate tokens. [ Note: Some white space is required to separate otherwise adjacent identifiers, keywords, numeric literals, and alternative tokens containing alphabetic characters. — end note ]
+"""
 
 
 
-t_ignore_COMMENT = r'[//.*|/*'
+tokens = (
+    'IDENTIFIERS',
+    'KEYWORDS',
+    'LITERALS',
+    'OPERATORS',
+    'SEPARATORS',
+)
 
+# All tokens must be named in advance.
+tokens = ( 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN',
+           'NAME', 'NUMBER' )
+
+# Ignored characters
+t_ignore = ' \t'
+
+# Token matching rules are written as regexs
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+# A function can be used if there is an associated action.
+# Write the matching regex in the docstring.
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Ignored token with an action associated with it
+def t_ignore_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count('\n')
+
+# Error handler for illegal characters
 def t_error(t):
-    print(f"Illegal character '{t.value[0]}'")
+    print(f'Illegal character {t.value[0]!r}')
     t.lexer.skip(1)
 
 
-lexer = lex.lex.lex()
+"""
+2.7 Comments
+
+The characters /* start a comment, which terminates with the characters */. These comments do not nest. The characters // start a comment, which terminates immediately before the next new-line character. If there is a form-feed or a vertical-tab character in such a comment, only white-space characters shall appear between it and the new-line that terminates the comment; no diagnostic is required. [ Note: The comment characters //, /*, and */ have no special meaning within a // comment and are treated just like other characters. Similarly, the comment characters // and /* have no special meaning within a /* comment.
+— end note ]
+"""
+
+
+input = '''
+ 3 + 4 * 10 + f
+   + -20 *2
+'''
+
+lexer = lex()
+lexer.input(input)
+
+for token in lexer:
+    print(token)
