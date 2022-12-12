b3df39db9c9c5a0633f32cdd2053b61caf196260
chenjz20 <843273746@qq.com>
2022-12-12 19:26:45 +0800

    fix

--- src/parser3/preprocessing.py
+++ src/parser3/preprocessing.py
@@ -7,8 +7,10 @@ def p_pp_include(p):
 
 def p_system_dir(p):
     ''' system_dir : '<' IDENTIFIER '>' '''
-    p[0] = ExternalNode('system_dir', p[2])
+    p[2] = ExternalNode('IDENTIFIER',p[2])
+    p[0] = InternalNode('system_dir', p[1:])
 
 def p_current_dir(p):
     ''' current_dir : '"' IDENTIFIER '"' '''
-    p[0] = ExternalNode('current_dir', p[2])
+    p[2] = ExternalNode('IDENTIFIER',p[2])
+    p[0] = InternalNode('current_dir', p[1:])
