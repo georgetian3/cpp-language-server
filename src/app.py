6c9ffde7f04ea065b0c5f199f136fde0253757d1
George Tian <github@georgetian.com>
2022-12-28 14:25:00 +0800

    editor

--- src/app.py
+++ src/app.py
@@ -1,25 +1,49 @@
 from flask import Flask, send_file, request
 from flask_cors import CORS
-
+import ply.lex as lex
+from ply.yacc import yacc
+from lexer.tokens import *
+from parser.myast import traverse
+from parser.parser import *
 
 app = Flask(__name__)
 CORS(app)
 
+lexer = lex.lex(debug=False)
+
+def get_tokens(src):
+    lexer.input(src)
+    return list(lexer)
+
+def run_parser(src):
+    parser = yacc(debug=False)
+    ast = parser.parse(src, debug=False)
+    tree = traverse(ast)
+    return tree
+
 
 @app.get('/')
 def index_get():
-    return send_file('editor.html')
+    return send_file('static/editor.html')
 
 @app.post('/process')
 def process():
     src = request.data.decode('utf8')
     print('Source:', src)
 
-    res = '<span style="color:red">' + src + '</span>'
+    res = '<span style="color:white">' + src + '</span>'
     suggestions = ['test', 'test1']
     print('Formatting:', res)
     print('Autocomplete:', suggestions)
     return {'formatting': res, 'suggestions': suggestions}
 
+src = """
+#include <iostream>
+
+int main() {
+    cout << "Hello world";
+}"""
+
 if __name__ == '__main__':
+    #print(get_tokens(src))
     app.run(debug=False, port='5000')
---
+++ src/static/editor.js
@@ -0,0 +1,50 @@
+function handle_update(element) {
+    fetch('http://127.0.0.1:5000/process', {
+        method: 'POST',
+        body: element.innerText
+    })
+    .then(response => response.json())
+    .then((data) => {
+        element.innerHTML = data.formatting;
+        let suggestions = document.getElementById('suggestions')
+        suggestions.innerHTML = '';
+        for (const suggestion of data.suggestions) {
+            console.log(suggestion);
+            let button = document.createElement('code');
+            button.setAttribute('class', 'suggestion');
+            button.setAttribute('onclick', 'autocomplete(this.innerText);');
+            button.innerText = suggestion;
+            suggestions.appendChild(button);
+        }
+    });
+}
+
+function register() {
+    console.log('here')
+    codeInput.registerTemplate("syntax-highlighted", codeInput.templates.custom(
+        handle_update,
+        true, /* Optional - Is the `pre` element styled as well as the `code` element? Changing this to false uses the code element as the scrollable one rather than the pre element */
+        true, /* Optional - This is used for editing code - setting this to true overrides the Tab key and uses it for indentation */
+        false /* Optional - Setting this to true passes the `<code-input>` element as a second argument to the highlight function to be used for getting data- attribute values and using the DOM for the code-input */,
+        [] // Array of plugins (see below)
+    ));
+}
+
+function autocomplete(text) {
+    let editor_parent = document.getElementById('editor');
+    let editor_textarea = editor_parent.getElementsByTagName('textarea')[0];
+    let editor_value = editor_parent.getElementsByTagName('pre')[0].getElementsByTagName('code')[0];
+    console.log(editor_parent, editor_textarea, editor_value);
+    editor_value.innerHTML += text;
+    editor_parent.value += text;
+    editor_textarea.dispatchEvent(new Event('input'));
+}
+/* document.addEventListener('keydown', function(key) {
+  if (key.keyCode != 9) { // not a tab key
+    return;
+  }
+  key.preventDefault();
+  key.stopPropagation();
+  console.log(key);
+  autocomplete();
+}); */
